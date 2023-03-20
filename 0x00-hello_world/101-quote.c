965e60539949d0126a3da0523486ceb86e0dcba2
Ernest <okiyaernest54@gmail.com>
2023-03-20 07:58:19 -0700

    Betty Style Updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 /**
- * main - prints a string to the standard error
+ * main - Entry point of the program
  *
- * Return: Always 1 (error)
+ * Description: This program prints a string to the standard error.
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
     char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     fprintf(stderr, "%s", str);
+
     return (1);
 }
