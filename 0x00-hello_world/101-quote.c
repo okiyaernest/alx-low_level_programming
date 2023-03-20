222c8dc8a4d8519b8938e72c1b003be73268c9d3
Ernest <okiyaernest54@gmail.com>
2023-03-20 07:50:50 -0700

    betty file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - prints a string to the standard error
+ *
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    fprintf(stderr, "%s", str);
+    return (1);
 }
