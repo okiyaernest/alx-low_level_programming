79c16de14d6fabe429473f867abf14fd670398da
Ernest <okiyaernest54@gmail.com>
2023-03-20 06:05:03 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%s\n", "A masterpiece of art is the expression of one's soul.");
+    printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
     return 0;
 }
