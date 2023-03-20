94415ea9fd15ddaab15bc130fcc091700df69f5b
Ernest <okiyaernest54@gmail.com>
2023-03-20 05:36:09 -0700

    updates

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,1 @@
+#include <stdio.h> int main(void) { printf("%s\n", "A masterpiece of art is the expression of one's soul."); return 0; }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+#include <stdio.h> int main(void) { printf("Size of char: %ld byte(s)\nSize of int: %ld byte(s)\nSize of long int: %ld byte(s)\nSize of long long int: %ld byte(s)\nSize of float: %ld byte(s)\nSize of double: %ld byte(s)\nSize of long double: %ld byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float), sizeof(double), sizeof(long double)); return 0; }
