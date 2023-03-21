d05801339e79bae15eecfa0fa9551c0aea20e558
Ernest <okiyaernest54@gmail.com>
2023-03-21 15:19:07 -0700

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,24 +7,33 @@
  */
 int main(void)
 {
-    char c = '_';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'p';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'u';
-    write(STDOUT_FILENO, &c, 1);
-    c = 't';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'c';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'h';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'a';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'r';
-    write(STDOUT_FILENO, &c, 1);
-    c = '\n';
-    write(STDOUT_FILENO, &c, 1);
+	char c = '_';
 
-    return (0);
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'p';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'u';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 't';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'c';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'h';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'a';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = 'r';
+	write(STDOUT_FILENO, &c, 1);
+
+	c = '\n';
+	write(STDOUT_FILENO, &c, 1);
+
+	return (0);
 }
