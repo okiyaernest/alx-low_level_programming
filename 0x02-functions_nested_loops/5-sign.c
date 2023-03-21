fccf64321500d274ca36f2a3e437e47afddf7b14
Ernest <okiyaernest54@gmail.com>
2023-03-21 14:49:10 -0700

    updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 
 /**
- * print_sign - Check Holberton
- * @n: An input number
- * Description: Tohis function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, and -1 if n is less than zero
  */
 int print_sign(int n)
 {
-	int value;
-
 	if (n > 0)
 	{
-		value = 1;
 		putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		value = 0;
 		putchar('0');
+		return (0);
 	}
 	else
 	{
-		value = -1;
 		putchar('-');
+		return (-1);
 	}
-
-	return (value);
 }
