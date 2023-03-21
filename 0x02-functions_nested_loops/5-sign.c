12a15a0eaab3f424b18befcd9a6a13793563a2da
Ernest <okiyaernest54@gmail.com>
2023-03-21 14:57:40 -0700

    updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,21 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
-	}
-	else
-	{
-		putchar('-');
-		return (-1);
-	}
+    if (n > 0)
+    {
+        putchar('+');
+        putchar('1');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        putchar('0');
+        return (0);
+    }
+    else
+    {
+        putchar('-');
+        putchar('1');
+        return (-1);
+    }
 }
