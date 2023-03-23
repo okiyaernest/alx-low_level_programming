7296b7c8fdf1a44e83787d11b3d60751e2bcdc35
Ernest <okiyaernest54@gmail.com>
2023-03-23 06:59:32 -0700

    updates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_line - prints a straight line in the terminal
+ * @n: the number of times the character '_' should be printed
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+void print_line(int n)
 {
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
+  int i;
+
+  if (n <= 0)
+  {
+    _putchar('\n');
+    return;
+  }
+
+  for (i = 0; i < n; i++)
+    _putchar('_');
+
+  _putchar('\n');
 }
