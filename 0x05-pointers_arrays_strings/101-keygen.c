c2d992fbde92d50423a02953f889188a75a2a61e
Ernest <okiyaernest54@gmail.com>
2023-03-27 07:46:49 -0700

    Advanced Updates

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+
+int main(void) {
+    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
+    const char charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const int charset_length = sizeof(charset) - 1;
+
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+
+    // Generate random characters for the password
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % charset_length];
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Add null terminator
+
+    printf("%s\n", password); // Print the generated password
+
+    return 0;
+}
