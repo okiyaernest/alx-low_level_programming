2b02e9b2e6bedd55070a835acef0fe8b0be79609
Ernest <okiyaernest54@gmail.com>
2023-03-27 08:06:27 -0700

    updates

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,22 @@
-#include <main.h>
+#include "main.h"
+#include <stdio.h>
 
-#define PASSWORD_LENGTH 6
+#define PASSWORD_LENGTH 10 // The length of the generated password
 
 int main(void) {
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
-    const char charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const int charset_length = sizeof(charset) - 1;
+  char password[PASSWORD_LENGTH + 1]; // The password buffer, plus one extra character for the null terminator
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // The characters allowed in the password
+  const int charset_size = sizeof(charset) - 1; // The size of the character set, minus one for the null terminator
+  srand(time(NULL)); // Seed the random number generator with the current time
 
-    // Seed the random number generator with the current time
-    srand(time(NULL));
+  // Generate a random password
+  for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    password[i] = charset[rand() % charset_size];
+  }
+  password[PASSWORD_LENGTH] = '\0'; // Add the null terminator to the end of the password
 
-    // Generate random characters for the password
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % charset_length];
-    }
-    password[PASSWORD_LENGTH] = '\0'; // Add null terminator
+  // Print the password
+  printf("%s\n", password);
 
-    printf("%s\n", password); // Print the generated password
-
-    return 0;
+  return 0;
 }
