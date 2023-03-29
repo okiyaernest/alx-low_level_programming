a5d703bdc03b000b3d509dd0bf262aca49200184
Ernest <okiyaernest54@gmail.com>
2023-03-29 16:53:42 -0700

    Advanced Updates

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,12 +16,9 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  ;
+  *(p + (2 * sizeof(int))) = 98;
   /* ...so that this prints 98\n */
-
-  *(p + (2 * sizeof(int))) = 98; // added line
-
+ 
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-
