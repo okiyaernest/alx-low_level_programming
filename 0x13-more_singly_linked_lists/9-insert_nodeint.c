ae400252646f045ff1a9b23c2776ad2b9de54e66
Ernest <okiyaernest54@gmail.com>
2023-05-01 23:19:44 -0700

    updates

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,48 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
+ * insert_nodeint_at_index - Inserts a new node at a given position in a
+ *                            linked list of listint_t nodes.
+ * @head: A pointer to a pointer to the head node of the linked list.
+ * @idx: The index of the list where the new node should be added.
+ * @n: The value to store in the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *new_node, *temp;
+    listint_t *new_node, *current;
+    unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
+    if (head == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->n = n;
-		return (new_node);
-	}
+    new_node->n = n;
+    new_node->next = NULL;
 
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		new_node->n = n;
-		*head = new_node;
-		return (new_node);
-	}
+    if (idx == 0)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
 
-	temp = *head;
-	while (i < idx)
-	{
-		temp = temp->next;
-		i++;
-	}
+    current = *head;
+    for (i = 0; i < idx - 1 && current != NULL; i++)
+        current = current->next;
 
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+    if (current == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
+
+    return (new_node);
 }
