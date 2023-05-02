2f082d4bb77d2619ea61b1cf81b459759bbb1168
Ernest <okiyaernest54@gmail.com>
2023-05-01 22:54:30 -0700

    update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,46 +1,40 @@
 #include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/**
  * find_listint_loop - Finds the loop in a linked list
- * @head: Pointer to the head of the list
- *
+ * @head: Pointer to the head of the linked list
  * Return: The address of the node where the loop starts, or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
+    listint_t *slow, *fast;
 
-    while (slow && fast && fast->next)
+    if (head == NULL || head->next == NULL)
+        return (NULL);
+
+    slow = head->next;
+    fast = head->next->next;
+
+    while (fast != NULL && fast->next != NULL)
     {
-        slow = slow->next;
-        fast = fast->next->next;
-
         if (slow == fast)
         {
+            /* Loop detected */
             slow = head;
-            while (slow && fast)
+
+            while (slow != fast)
             {
-                if (slow == fast)
-                    return (slow);
                 slow = slow->next;
                 fast = fast->next;
             }
+
+            return (slow);
         }
+
+        slow = slow->next;
+        fast = fast->next->next;
     }
 
+    /* No loop detected */
     return (NULL);
 }
